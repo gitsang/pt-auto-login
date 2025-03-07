890751569d1be1fb45c5573a2ee1b2786d118ea1
sang <sang.chen@outlook.com>
2025-03-07 11:07:30 +0800

    fmt: format code

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import argparse
 import os
 import pyotp
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s"
+)
 
-def check_login(driver = None):
+
+def check_login(driver=None):
     if driver is None:
         driver = DriverUtils.get_driver()
     try:
@@ -17,6 +20,7 @@ def check_login(driver = None):
     except:
         return False
     return True
+
 
 def login(driver):
     # 1. open login page
@@ -32,7 +36,7 @@ def login(driver):
 
     # 3. click login button
     span_element = driver.find_element(By.XPATH, "//span[text()='登 錄']")
-    login_button= span_element.find_element(By.XPATH, "./ancestor::button")
+    login_button = span_element.find_element(By.XPATH, "./ancestor::button")
     login_button.click()
 
     # 4. check if need otp code
@@ -41,14 +45,14 @@ def login(driver):
     else:
         # 4.1 input otp code
         logging.info("verifing otp...")
-        otp_code_input= driver.find_element(By.ID, "otpCode")
+        otp_code_input = driver.find_element(By.ID, "otpCode")
         totp = pyotp.TOTP(totp_sec)
         totp_code = totp.now()
         otp_code_input.send_keys(totp_code)
 
         # 4.2 click verify button
         span_element = driver.find_element(By.XPATH, "//span[text()='登 錄']")
-        login_button= span_element.find_element(By.XPATH, "./ancestor::button")
+        login_button = span_element.find_element(By.XPATH, "./ancestor::button")
         login_button.click()
 
         # 4.3 recheck if login success
@@ -61,23 +65,30 @@ def login(driver):
     logging.info("closing driver...")
     DriverUtils.close_driver(driver)
 
+
 if __name__ == "__main__":
     # parse command line arguments
-    parser = argparse.ArgumentParser(description='Login script')
-    parser.add_argument('--base_url', type=str, help='Base URL for login')
-    parser.add_argument('--username', type=str, help='Username for login')
-    parser.add_argument('--password', type=str, help='Password for login')
-    parser.add_argument('--totp_sec', type=str, help='TOTP secret for OTP')
-    parser.add_argument('--headless', type=bool, default=True, help='Enable headless mode')
+    parser = argparse.ArgumentParser(description="Login script")
+    parser.add_argument("--base_url", type=str, help="Base URL for login")
+    parser.add_argument("--username", type=str, help="Username for login")
+    parser.add_argument("--password", type=str, help="Password for login")
+    parser.add_argument("--totp_sec", type=str, help="TOTP secret for OTP")
+    parser.add_argument(
+        "--headless", type=bool, default=True, help="Enable headless mode"
+    )
     args = parser.parse_args()
 
     # load environment variables
     load_dotenv()
-    base_url = args.base_url or os.getenv('BASE_URL') or ""
-    username = args.username or os.getenv('USERNAME') or ""
-    password = args.password or os.getenv('PASSWORD') or ""
-    totp_sec = args.totp_sec or os.getenv('TOTP_SEC') or ""
-    headless = args.headless if args.headless is not None else (os.getenv('HEADLESS', 'True').lower() in ['true', '1', 't'])
+    base_url = args.base_url or os.getenv("BASE_URL") or ""
+    username = args.username or os.getenv("USERNAME") or ""
+    password = args.password or os.getenv("PASSWORD") or ""
+    totp_sec = args.totp_sec or os.getenv("TOTP_SEC") or ""
+    headless = (
+        args.headless
+        if args.headless is not None
+        else (os.getenv("HEADLESS", "True").lower() in ["true", "1", "t"])
+    )
 
     # init driver
     driver = DriverUtils.get_driver(headless=headless)
